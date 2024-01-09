5c26976803d88be84b2b53f83a0b457c23a44669
jobs-rw <mjabush@yahoo.fr>
2024-01-09 16:42:59 +0000

    debug

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,12 +1,10 @@
-function getListStudentIds(students) {
+const getListStudentIds = (students) => {
+  // Check if the argument is an array
   if (!Array.isArray(students)) {
     return [];
   }
-
-  // Use the map function to extract the 'id' property from each object
   return students.map(student => student.id);
-}
+};
 const students = getListStudents();
 const studentIds = getListStudentIds(students);
 console.log(studentIds);
-
